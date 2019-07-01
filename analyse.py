6d7092df582a25f65ff6b8281134b5d5feece449
Johannes Buchner <johannes.buchner.acad@gmx.com>
2019-07-01 14:05:24 +0200

    python3 compatibility

--- analyse.py
+++ analyse.py
@@ -2,18 +2,18 @@ import astropy.io.fits as pyfits
 import sys
 
 filename = sys.argv[1]
-print 'file:', filename
+print(('file:', filename))
 f = pyfits.open(filename)
+s = f['SPECTRUM']
 
 telescope = s.header['TELESCOP']
-print 'telescope: %s' % telescope
+print(('telescope: %s' % telescope))
 instrument = s.header['INSTRUME']
-print 'instrument: %s' % instrument
+print(('instrument: %s' % instrument))
 
-s = f['SPECTRUM']
-print 'exposure time: %.1f seconds' % s.header['EXPOSURE']
+print(('exposure time: %.1f seconds' % s.header['EXPOSURE']))
 y = s.data['COUNTS']
-print 'counts: %d' % y.sum()
+print(('counts: %d' % y.sum()))
 
 
 if y.sum() > 1000:
